6f5d141d1e78b0df657fda425a0bbba4f984bb29
Addison Crosbie <abacrosbie@gmail.com>
2025-06-07 10:46:04 +0100

    added operator and operate function to js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,46 @@
-//number and operator variables
-let num1 = '';
-let num2 = '';
-let operator = '';
+//Operator Functions
+//add
+function addFunct(num1, num2) {
+    return num1 + num2;
+}
+
+//subtract
+function minusFunct(num1, num2) {
+    return num1 + num2;
+}
+
+//multiply
+function multFunct(num1, num2) {
+    return num1 * num2;
+}
+
+//divide
+function divFunct(num1, num2) {
+    return num1 / num2;
+}
+
+//operate function
+function operate(n1, n2, operatorFunc) {
+    let result;
+    switch(operate) {
+        case '+': {
+            result = addFunct(n1, n2);
+            break;
+            }
+        case '-': {
+            result = minusFunct(n1, n2);
+            break;
+            }   
+        case '*': {
+            result = multFunct(n1, n2);
+            break;
+            }
+        case '/': {
+            result = divFunct(n1, n2);
+            }
+        }
+    return result
+}
+
+
 
