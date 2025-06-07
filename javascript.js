3265fa96ca7204b3e46da848b58ab7dad4d02c8a
Addison Crosbie <abacrosbie@gmail.com>
2025-06-07 11:06:07 +0100

    tested and fixed operate function, added 0 errors to divide function

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ function addFunct(num1, num2) {
 
 //subtract
 function minusFunct(num1, num2) {
-    return num1 + num2;
+    return num1 - num2;
 }
 
 //multiply
@@ -16,13 +16,19 @@ function multFunct(num1, num2) {
 
 //divide
 function divFunct(num1, num2) {
-    return num1 / num2;
+    if (num2 === 0) {
+        return ">.< Error, don't break the universe.";
+    }
+    else if (num1 === 0) {
+        return 0;
+    }
+    else return num1 / num2;
 }
 
 //operate function
 function operate(n1, n2, operatorFunc) {
     let result;
-    switch(operate) {
+    switch(operatorFunc) {
         case '+': {
             result = addFunct(n1, n2);
             break;
@@ -44,3 +50,4 @@ function operate(n1, n2, operatorFunc) {
 
 
 
+console.log(operate(100, 0, '/'));
